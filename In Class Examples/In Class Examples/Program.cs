620074d602c8d05cb3b68010137037978e50b226
Matthew Lauer <mjlauer@ou.edu>
2021-02-02 18:33:06 -0600

    Wrote a code ish thingy

--- In Class Examples/In Class Examples/Program.cs
+++ In Class Examples/In Class Examples/Program.cs
@@ -6,7 +6,24 @@ namespace In_Class_Examples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstname, lastname;
+            double gpa;
+
+            Console.WriteLine("Please enter your first name>>");
+            firstname = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name >>");
+            lastname = Console.ReadLine();
+
+            Console.WriteLine("Please enter your gpa >>");
+            string gpaAsString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaAsString);
+
+            gpa = gpa + .5; // This is a comment
+
+            string output = "Hello" + firstname " " + lastname "! " + " We boosted your gpa " + gpa.ToString("n3");
+
+            Console.WriteLine(output);
         }
     }
 }
