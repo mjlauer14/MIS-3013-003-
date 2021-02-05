8fc0e555b424801898b9f0c4cb3651f0ac527d40
Matthew Lauer <mjlauer@ou.edu>
2021-02-04 18:45:42 -0600

    Finshed code

---
+++ Participation feb 4/Participation feb 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Participation_feb_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SumOf3/SumOf3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumOf3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const Double MAGIC_NUMBER = 7.777;//local constant variable
+            double number_1;
+            double number_2;
+            double number_3;
+            double SUM;
+            double Product;
+
+
+            Console.WriteLine("Enter a number");
+            number_1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number");
+            number_2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a third number");
+            number_3 = Convert.ToDouble(Console.ReadLine());
+
+            SUM = number_1 + number_2 + number_3;
+            Console.WriteLine("The sum is" + SUM.ToString("N3"));
+
+            Product = MAGIC_NUMBER * SUM;
+
+            Console.WriteLine("The product is" + Product.ToString("N3"));
+        }
+    }
+}
